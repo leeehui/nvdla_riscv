a81de7f7e1b5bcbf64abd48aa959683b66898a0a
LiHui <leehui_hrbeu71102@163.com>
2018-09-13 16:46:52 +0800

    add interrupt dummy files

---
+++ src/interrupt.c
@@ -0,0 +1,33 @@
+
+void ISR_WDT (void)
+{
+	/*
+	1, clear int
+	2, record wdt status
+	*/
+
+	//IEU_INT_CLEAR_PENDING |= INT_WDT_EN;
+    //do some thing
+}
+
+void ISR_TIMER (void)
+{
+	/*
+	1, clear int
+	2, record wdt status
+	*/
+
+	//IEU_INT_CLEAR_PENDING |= INT_WDT_EN;
+    //do some thing
+}
+
+void ISR_NVDLA (void)
+{
+	/*
+	1, clear int
+	2, record wdt status
+	*/
+
+	//IEU_INT_CLEAR_PENDING |= INT_WDT_EN;
+    //do some thing
+}
