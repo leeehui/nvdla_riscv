c8e362d0cc57582adabb18267af55178ec9b84c8
lihui <hui.li@hxt-semitech.com>
2018-12-29 14:08:51 +0800

    optimize scripts, change dir and file names

--- build.sh
+++ build.sh
@@ -5,22 +5,28 @@ mkdir -p build
 cd build
 cmake -D PLAT=$1 -D BIN_NAME=$2 ../src
 make
-riscv64-unknown-elf-objdump -d nvdla.elf > ./nvdla.dump
-riscv64-unknown-elf-objcopy -O binary -R .note -R .comment -S nvdla.elf nvdla.bin
 
-mkdir output
-python ../convert.py -i nvdla.bin
-python ../convert-ipdv.py -i nvdla.bin
+if [ $# -eq 3 ]; then
 
-cd output
-dir=$(date +'%Y%m%d-%H%M%S')
-dir_ipdv="${dir}_ipdv"
-mkdir $dir
-mkdir $dir_ipdv
+    if [ $3 =  "bin" ]; then
+        riscv64-unknown-elf-objdump -d nvdla.elf > ./nvdla.dump
+        riscv64-unknown-elf-objcopy -O binary -R .note -R .comment -S nvdla.elf nvdla.bin
 
-cp gpsram_blk_*.hex $dir
-cp nvdla.bin.dat  "${dir_ipdv}/nvdla.${2}.dat"
-cp ../nvdla.dump "${dir_ipdv}/nvdla.${2}.dump"
-cp ../nvdla.elf  "${dir_ipdv}/nvdla.${2}.elf"
-cp $dir /mnt/share -r
-cp $dir_ipdv /mnt/share -r
+        mkdir output
+        python ../convert.py -i nvdla.bin
+        python ../convert-ipdv.py -i nvdla.bin
+
+        cd output
+        dir=$(date +'%Y%m%d-%H%M%S')
+        dir="${dir}.${1}_${2}"
+        mkdir $dir
+
+        cp gpsram_blk_*.hex $dir
+        cp nvdla.bin.dat  "${dir}/${dir}.dat"
+        cp ../nvdla.dump "${dir}/${dir}.dump"
+        cp ../nvdla.elf  "${dir}/${dir}.elf"
+        cp $dir /mnt/share -r
+    else
+        echo "invalid parameter 3"
+    fi
+fi
