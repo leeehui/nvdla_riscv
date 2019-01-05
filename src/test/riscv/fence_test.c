8af1dfc32ceb4966b0fcacb0cecfd09c147eb4b2
lihui <hui.li@hxt-semitech.com>
2019-01-05 16:07:47 +0800

    add fence_test: try to clarify how fence handle i o r w

---
+++ src/test/riscv/fence_test.c
@@ -0,0 +1,18 @@
+
+#include "femto.h"
+#include "test_util.h"
+
+
+void riscv_runtime(void)
+{
+    gpsram_write32(SIM_SIGNAL_ADDR, 0x5a5a5a5a);
+    __asm__ __volatile__ ("fence w,o" :::"memory");
+    *(volatile uint32_t*)(uintptr_t)(ARIANE_CSR_INTR_IN_TRIG) = 0x1;
+
+    gpsram_write32(SIM_SIGNAL_ADDR+4, 0x5a5a5a5a);
+    __asm__ __volatile__ ("fence w,w" :::"memory");
+    *(volatile uint32_t*)(uintptr_t)(ARIANE_CSR_INTR_IN_TRIG) = 0x1;
+
+    signal_to_simulation(0xaa);
+
+}
