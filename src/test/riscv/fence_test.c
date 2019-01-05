1eaaf20e60ef39d6e458da18d4594faea147e94d
lihui <hui.li@hxt-semitech.com>
2019-01-05 17:25:40 +0800

    add more for fence_test

--- src/test/riscv/fence_test.c
+++ src/test/riscv/fence_test.c
@@ -5,11 +5,27 @@
 
 void riscv_runtime(void)
 {
-    gpsram_write32(SIM_SIGNAL_ADDR, 0x5a5a5a5a);
+    gpsram_write32(DLA_BASE, 0x5a5a5a5a);
     __asm__ __volatile__ ("fence w,o" :::"memory");
     *(volatile uint32_t*)(uintptr_t)(ARIANE_CSR_INTR_IN_TRIG) = 0x1;
 
-    gpsram_write32(SIM_SIGNAL_ADDR+4, 0x5a5a5a5a);
+    gpsram_write32(DLA_BASE+0x100, 0x5a5a5a5a);
+    __asm__ __volatile__ ("fence w,w" :::"memory");
+    *(volatile uint32_t*)(uintptr_t)(ARIANE_CSR_INTR_IN_TRIG) = 0x1;
+
+    gpsram_write32(DLA_BASE+0x200, 0x5a5a5a5a);
+    __asm__ __volatile__ ("fence w,o" :::"memory");
+    *(volatile uint32_t*)(uintptr_t)(ARIANE_CSR_INTR_IN_TRIG) = 0x1;
+
+    gpsram_write32(DLA_BASE+0x300, 0x5a5a5a5a);
+    __asm__ __volatile__ ("fence w,w" :::"memory");
+    *(volatile uint32_t*)(uintptr_t)(ARIANE_CSR_INTR_IN_TRIG) = 0x1;
+
+    gpsram_write32(DLA_BASE+0x400, 0x5a5a5a5a);
+    __asm__ __volatile__ ("fence w,o" :::"memory");
+    *(volatile uint32_t*)(uintptr_t)(ARIANE_CSR_INTR_IN_TRIG) = 0x1;
+
+    gpsram_write32(DLA_BASE+0x500, 0x5a5a5a5a);
     __asm__ __volatile__ ("fence w,w" :::"memory");
     *(volatile uint32_t*)(uintptr_t)(ARIANE_CSR_INTR_IN_TRIG) = 0x1;
 
