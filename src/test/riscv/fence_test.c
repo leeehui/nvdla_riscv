d6417f748bb0fc9abd3948e6a47dac03bd6bef16
lihui <hui.li@hxt-semitech.com>
2019-01-07 17:36:33 +0800

    change fence_test

--- src/test/riscv/fence_test.c
+++ src/test/riscv/fence_test.c
@@ -5,6 +5,14 @@
 
 void riscv_runtime(void)
 {
+    gpsram_write32(SIM_SIGNAL_ADDR, 0x5a5a5a5a);
+    __asm__ __volatile__ ("fence w,w" :::"memory");
+    *(volatile uint32_t*)(uintptr_t)(ARIANE_CSR_INTR_IN_TRIG) = 0x1;
+
+    gpsram_write32(SIM_SIGNAL_ADDR+4, 0x5a5a5a5a);
+    __asm__ __volatile__ ("fence w,o" :::"memory");
+    *(volatile uint32_t*)(uintptr_t)(ARIANE_CSR_INTR_IN_TRIG) = 0x1;
+
     gpsram_write32(DLA_BASE, 0x5a5a5a5a);
     __asm__ __volatile__ ("fence w,o" :::"memory");
     *(volatile uint32_t*)(uintptr_t)(ARIANE_CSR_INTR_IN_TRIG) = 0x1;
